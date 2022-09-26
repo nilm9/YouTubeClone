54c8a9f21ed2b623eea2cd353943017b001012ac
Nil Monfort <nil.monfort.vila@estudiants.urv.cat>
2022-09-26 12:38:31 +0200

    files uploaded

---
+++ src/components/Videos.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import VideoCard from './VideoCard';
+import ChannelCard from './ChannelCard';
+import { Stack } from '@mui/material';
+import {Box} from '@mui/material';
+
+const Videos = ({videos}) => {
+  console.log(videos);
+  return (
+       <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+        {videos.map((item, idx) => (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        ))}
+       </Stack>
+    
+  )
+}
+
+export default Videos
